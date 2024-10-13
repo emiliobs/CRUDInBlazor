04bc0c61a39acb3f6e05ba95700cd9285820b265
Emilio Barrera <barrera_emilio@yahoo.es>
2024-10-13 19:45:50 +0100

    app done

--- CRUDInBlazor/Program.cs
+++ CRUDInBlazor/Program.cs
@@ -1,7 +1,6 @@
 ﻿using CRUDInBlazor.Components;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using CRUDInBlazor.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUDInBlazor
 {
@@ -29,7 +28,7 @@ namespace CRUDInBlazor
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-    app.UseMigrationsEndPoint();
+                app.UseMigrationsEndPoint();
             }
 
             app.UseHttpsRedirection();
